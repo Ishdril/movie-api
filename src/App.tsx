f0f1009eab4d86d383143a2d2d89f75dfc869269
Ishdril <berni2015@gmail.com>
2021-06-02 19:51:46 +0200

    feat: basic css and connect app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import Footer from './components/Footer/Footer';
+import MovieList from './components/MovieList/MovieList';
+import Navbar from './components/Navbar/Navbar';
+import SearchResult from './interfaces/searchResult';
+import { getDiscoverMovies } from './services/api';
 
 function App() {
-  return <div className="App">hello world!</div>;
+  const [movies, setMovies] = useState<SearchResult[]>([]);
+  useEffect(() => {
+    getDiscoverMovies().then(movies => {
+      setMovies(movies);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <Navbar />
+      <MovieList movieList={movies} />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
