9ea06a83a2fc36ae79bd0504830ca4a7da39dd78
Ishdril <berni2015@gmail.com>
2021-06-02 19:50:15 +0200

    feat: parse movies to assign image urls

---
+++ src/helpers/parseMovies.ts
@@ -0,0 +1,13 @@
+import SearchResult from '../interfaces/searchResult';
+
+const baseImageURL = process.env['REACT_APP_IMAGE_URL'] as string;
+const noImage = process.env['REACT_APP_NO_IMAGE'] as string;
+
+const parseMovies = (movies: SearchResult[]): SearchResult[] => {
+  return movies.map(movie => {
+    movie.poster_path = movie.poster_path ? baseImageURL + movie.poster_path : noImage;
+    movie.backdrop_path = movie.backdrop_path ? baseImageURL + movie.backdrop_path : noImage;
+    return movie;
+  });
+};
+export default parseMovies;
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,17 +1,19 @@
 import Search, { defaultSearch, isSearch } from '../interfaces/search';
+import parseMovies from '../helpers/parseMovies';
+import SearchResult from '../interfaces/searchResult';
 // import SearchResult from '../interfaces/searchResult';
 
 const apiKey = process.env['REACT_APP_API_KEY'];
 const apiURL = process.env['REACT_APP_API_URL'];
 
-const getDiscoverMovies = async (): Promise<Search> => {
+const getDiscoverMovies = async (): Promise<SearchResult[]> => {
   const res = await fetchFactory<Search>('/discover/movie');
-  return isSearch(res) ? res : defaultSearch;
+  return isSearch(res) ? parseMovies(res.results) : defaultSearch.results;
 };
 
-const searchMovies = async (searchStr: string): Promise<Search> => {
+const searchMovies = async (searchStr: string): Promise<SearchResult[]> => {
   const res = await fetchFactory<Search>('/search/movie', {}, `&query=${searchStr}`);
-  return isSearch(res) ? res : defaultSearch;
+  return isSearch(res) ? parseMovies(res.results) : defaultSearch.results;
 };
 
 // custom fetch function to easily create new requests to the API
@@ -19,11 +21,11 @@ const fetchFactory = async <T>(
   path: string,
   options?: RequestInit,
   query: string = ''
-): Promise<T | Response | false> => {
+): Promise<T | false> => {
   try {
     const res = await fetch(apiURL + path + `?api_key=${apiKey}${query}`, options);
     if (res.status >= 400) throw Error(`API responded with a ${res.status} (${res.statusText})`);
-    return res.status !== 204 ? ((await res.json()) as T) : res;
+    return (await res.json()) as T;
   } catch (error) {
     console.error(`Error fetching [${(options && options.method) || 'GET'}] ${path}: `, error);
     return false;
