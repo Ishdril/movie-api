a15d502b480d11689c980264567e2f583f81587d
Ishdril <berni2015@gmail.com>
2021-06-03 20:38:53 +0200

    fix: bug that prevented details from showing when clicking on the title

--- src/components/MovieTile/MovieTile.tsx
+++ src/components/MovieTile/MovieTile.tsx
@@ -2,7 +2,7 @@ import SearchResult from '../../interfaces/searchResult';
 import addFav from '../../assets/addFav.svg';
 import removeFav from '../../assets/removeFav.svg';
 import styles from './MovieTile.module.css';
-import { useContext } from 'react';
+import { MouseEvent, useContext } from 'react';
 import favContext from '../../services/FavContext';
 import MovieDetailsContext from '../../services/MovieDetailsContext';
 
@@ -14,20 +14,23 @@ const MovieTile = ({ movie }: MovieTileProps) => {
   const { favDictionary, favHandler } = useContext(favContext);
   const { movieDetailsHandler } = useContext(MovieDetailsContext);
 
+  const clickFavHandler = (e: MouseEvent): void => {
+    e.stopPropagation();
+    favHandler(movie);
+  };
+
   return (
     <div className={styles['movie-container']}>
       <div
         className={styles['movie']}
         style={{ backgroundImage: `url(${movie.backdrop_path})` }}
-        onClick={event => {
-          if (event.currentTarget === event.target) {
-            movieDetailsHandler(movie.id);
-          }
+        onClick={() => {
+          movieDetailsHandler(movie.id);
         }}
       >
         <div className={styles['movie__title']}>{movie.title}</div>
         {
-          <button className={styles['movie__fav-btn']} onClick={() => favHandler(movie)}>
+          <button className={styles['movie__fav-btn']} onClick={clickFavHandler}>
             <img
               src={favDictionary[movie.id] ? removeFav : addFav}
               alt={favDictionary[movie.id] ? 'remove from favourites' : 'add to favourites'}
